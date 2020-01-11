c35c2b89fdf58e0ce84eabeae8bc054e55516812
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-11 01:32:36 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,20 @@
+$phrase = "Mario"
+
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  puts "It's-a me, #{$phrase}!"
 end
 
 def toadstool
-  puts status
+  puts status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 end
+
+def link 
+  puts status = "It's Dangerous To Go Alone! Take This." 
+end 
+
+phrase = "Do A Barrel Roll!"
+def any_phrase(phrase)
+  puts "#{phrase}"
+end   
+  
