26ab3edbecaadedcf050227e7851c5b62664500b
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-11 01:27:35 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,7 +13,7 @@ def link
   puts status = "It's Dangerous To Go Alone! Take This." 
 end 
 
-phrase = "Do A Barrell Roll!"
+phrase = "Do A Barrell Roll"
 def any_phrase(phrase)
   puts status = "Do A Barrel Roll #{phrase}!"
 end   
